728c483cc1df7a0b9dd0e61b5f5bcf79873957bc
David Ventura <david@davidv.dev>
2025-12-29 00:11:30 +0100

    optimize clang usage with pch, remove linker

--- backend/src/compile.rs
+++ backend/src/compile.rs
@@ -1,35 +1,22 @@
-use serde::Serialize;
 use std::io::Write;
 use std::process::{Command, Stdio};
 use std::time::Instant;
 
-#[derive(Serialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct ProcessError {
-    stdout: String,
-    stderr: String,
-}
-
-#[derive(Serialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub enum InputProcessingError {
-    CompilerError(ProcessError),
-    StripError(ProcessError),
-}
-
-pub fn compile(source: &[u8]) -> Result<Vec<u8>, InputProcessingError> {
+pub fn compile(source: &[u8]) -> Result<Vec<u8>, String> {
     let s = Instant::now();
 
-    let mut clang = Command::new("clang")
+    let mut clang = Command::new("/home/david/git/ebpf-playground/clang/bin/clang")
         .args([
             "-g",
+            "-gmodules",
             "-O2",
             "-target",
             "bpf",
             "-D__TARGET_ARCH_x86",
-            "-I.",
+            "-include-pch",
+            "task.h.pch", // pch+gmodules means
+            // symbols are not part of this
             "-I/usr/include/bpf",
-            "-I/usr/include",
             "-x",
             "c",
             "-c",
@@ -47,42 +34,14 @@ pub fn compile(source: &[u8]) -> Result<Vec<u8>, InputProcessingError> {
     let output = clang.wait_with_output().unwrap();
 
     if !output.status.success() {
-        return Err(InputProcessingError::CompilerError(ProcessError {
-            stdout: String::from_utf8_lossy(&output.stdout).to_string(),
-            stderr: String::from_utf8_lossy(&output.stderr).to_string(),
-        }));
+        return Err(String::from_utf8_lossy(&output.stderr).to_string());
     }
 
-    println!("Compile itself took {:?}", s.elapsed());
-
-    let compiled_bytes = output.stdout;
-
-    let mut strip = Command::new("llvm-strip")
-        .args(["-g", "-", "-o", "-"])
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    strip
-        .stdin
-        .as_mut()
-        .unwrap()
-        .write_all(&compiled_bytes)
-        .unwrap();
-    let strip_output = strip.wait_with_output().unwrap();
-
-    if !strip_output.status.success() {
-        eprintln!(
-            "llvm-strip failed: {}",
-            String::from_utf8_lossy(&strip_output.stderr)
-        );
-        return Err(InputProcessingError::StripError(ProcessError {
-            stdout: String::from_utf8_lossy(&strip_output.stdout).to_string(),
-            stderr: String::from_utf8_lossy(&strip_output.stderr).to_string(),
-        }));
-    }
-
-    Ok(strip_output.stdout)
+    println!(
+        "Compiling {} bytes to {} bytes took {:?}",
+        source.len(),
+        output.stdout.len(),
+        s.elapsed()
+    );
+    Ok(output.stdout)
 }
