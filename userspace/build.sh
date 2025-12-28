cd058c4fc8fecfa9191caf008d26de1c2ade8d5f
David Ventura <david@davidv.dev>
2025-12-29 00:11:56 +0100

    build release

--- userspace/build.sh
+++ userspace/build.sh
@@ -1,7 +1,7 @@
 set -e
-LIBBPF_SYS_EXTRA_CFLAGS="-idirafter /usr/include/x86_64-linux-gnu -idirafter /usr/include" cargo build --target x86_64-unknown-linux-musl
-strip target/x86_64-unknown-linux-musl/debug/userspace
+LIBBPF_SYS_EXTRA_CFLAGS="-idirafter /usr/include/x86_64-linux-gnu -idirafter /usr/include" cargo build --release --target x86_64-unknown-linux-musl
+strip target/x86_64-unknown-linux-musl/release/userspace
 set -x
-cp target/x86_64-unknown-linux-musl/debug/userspace ../rootfs/main
+cp target/x86_64-unknown-linux-musl/release/userspace ../rootfs/main
 cd .. && mkfs.ext4 -F -d rootfs rootfs.ext4 
 
