3721f13329486dd4a0f65662cf42ea782196a4cc
David Ventura <david@davidv.dev>
2025-12-30 17:36:49 +0100

    add DEBUG_NUM

--- backend/includes/ep_platform.h
+++ backend/includes/ep_platform.h
@@ -1,9 +1,9 @@
 #include "task.h"
+
 #if __TINYC__
 #define __builtin_classify_type(a) 6
 #define __builtin_preserve_access_index(x) x
 #endif
-
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 
@@ -21,8 +21,6 @@ struct {
     __uint(max_entries, 16384);
 } _ep_debug_events SEC(".maps");
 
-
-
 #define DEBUG_STRUCT(label_str, struct_val) do { \
     _Static_assert(__builtin_classify_type(struct_val) != 5, "need a struct by value"); \
     _Static_assert(sizeof(struct_val) < 256, "Struct is too big"); \
@@ -32,6 +30,11 @@ struct {
 #define DEBUG_STR(label_str, str_val) do { \
     _Static_assert(sizeof(str_val) < 256, "String is too big"); \
     __debug_str(label_str, __COUNTER__, &str_val, sizeof(str_val)); \
+} while (0)
+
+#define DEBUG_NUM(label_str, num_val) do { \
+    _Static_assert(__builtin_classify_type(num_val) != 5, "need a number by value"); \
+    __debug_num(label_str, __COUNTER__, &num_val, sizeof(num_val)); \
 } while (0)
 
 static __always_inline void __ep_debug_val(const char *label, __u8 counter, void *ptr, size_t size, __u8 type) {
@@ -54,4 +57,17 @@ static __always_inline void __debug_str(const char *label, __u8 counter, void *p
 static __always_inline void __debug_struct(const char *label, __u8 counter, void *ptr, size_t size) {
     __ep_debug_val(label, counter, ptr, size, 4);
 }
+static __always_inline void __debug_num(const char *label, __u8 counter, void* num, size_t size) {
+    if (size > 8) size = 8; // no u128 for you sorry
+    char* buf = bpf_ringbuf_reserve(&_ep_debug_events, 8+3, 0);
+    if (!buf) {
+	bpf_printk("bpf_ringbuf_reserve failed\n");
+	return;
+    }
 
+    buf[0] = 2; // num i guess
+    buf[1] = counter;
+    buf[2] = size & 0xFF;
+    bpf_probe_read_kernel(buf + 3, size, num);
+    bpf_ringbuf_submit(buf, 0);
+}
