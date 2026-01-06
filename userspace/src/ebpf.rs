e7e7fa0862ccba4556f687f824d0d5d9fde90af5
David Ventura <david@davidv.dev>
2026-01-06 11:46:45 +0100

    refactor exercises to split load+run

--- userspace/src/ebpf.rs
+++ userspace/src/ebpf.rs
@@ -1,9 +1,8 @@
-use libbpf_rs::{MapCore, MapType, ObjectBuilder, PrintLevel, RingBuffer, RingBufferBuilder};
+use libbpf_rs::{Link, MapCore, MapType, ObjectBuilder, PrintLevel, RingBuffer, RingBufferBuilder};
 use libbpf_sys::{LIBBPF_STRICT_ALL, libbpf_set_strict_mode};
 use shared::GuestMessage;
 use std::sync::Mutex;
 use std::sync::atomic::Ordering;
-use std::sync::mpsc::Sender;
 use std::time::{Duration, Instant};
 
 static VERIFIER_LOG: Mutex<String> = Mutex::new(String::new());
@@ -16,88 +15,98 @@ fn capturing_printer(lvl: PrintLevel, s: String) {
     }
 }
 
-pub fn run_program(program: &[u8], timeout: Duration, tx: Sender<GuestMessage>) {
-    unsafe {
-        libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
-    }
+pub struct ProgramDetails {
+    pub name: String,
+    pub section: String,
+}
+pub struct EbpfLoader<'a> {
+    pb: RingBuffer<'a>,
+    _links: Vec<Link>,
+    pub program_details: Vec<ProgramDetails>,
+}
 
-    if let Ok(mut log) = VERIFIER_LOG.lock() {
-        log.clear();
-    }
-
-    libbpf_rs::set_print(Some((PrintLevel::Info, capturing_printer)));
-
-    let open_obj = match ObjectBuilder::default().open_memory(program) {
-        Ok(o) => o,
-        Err(_) => {
-            let captured_log = VERIFIER_LOG.lock().unwrap().clone();
-            tx.send(GuestMessage::LoadFail(captured_log)).unwrap();
-            return;
+impl<'a> EbpfLoader<'a> {
+    pub fn load_program<F>(program: &[u8], ev_closure: F) -> Result<EbpfLoader<'a>, GuestMessage>
+    where
+        F: Fn(&[u8]) -> i32 + 'a,
+    {
+        unsafe {
+            libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
         }
-    };
 
-    let mut obj = match open_obj.load() {
-        Ok(o) => o,
-        Err(_) => {
-            let captured_log = VERIFIER_LOG.lock().unwrap().clone();
-            tx.send(GuestMessage::VerifierFail(captured_log)).unwrap();
-            return;
+        if let Ok(mut log) = VERIFIER_LOG.lock() {
+            log.clear();
         }
-    };
 
-    let mut links: Vec<_> = Vec::new();
+        libbpf_rs::set_print(Some((PrintLevel::Info, capturing_printer)));
 
-    for p in obj.progs_mut() {
-        tx.send(GuestMessage::FoundProgram {
-            name: p.name().to_string_lossy().to_string(),
-            section: p.section().to_string_lossy().to_string(),
-        })
-        .unwrap();
-        let link = p.attach().unwrap(); // TODO
-        links.push(link);
-    }
-    if links.len() == 0 {
-        tx.send(GuestMessage::NoProgramsFound).unwrap();
-        return;
-    }
-
-    let mut pb: Option<RingBuffer> = None;
-
-    for m in obj.maps_mut() {
-        if m.map_type() == MapType::RingBuf && m.name().to_string_lossy() == "_ep_debug_events" {
-            let txer = tx.clone();
-            let r_closure = move |data: &[u8]| {
-                txer.send(GuestMessage::Event(data.into())).unwrap();
-                0
-            };
-
-            let mut rpb = RingBufferBuilder::new();
-            rpb.add(&m, r_closure).unwrap();
-            pb = Some(rpb.build().unwrap());
-            break;
-        }
-    }
-
-    let pb = if let Some(pb) = pb {
-        pb
-    } else {
-        tx.send(GuestMessage::DebugMapNotFound).unwrap();
-        return;
-    };
-
-    let start = Instant::now();
-    crate::EBPF_READY.store(true, Ordering::Relaxed);
-    while start.elapsed() < timeout && !crate::SHOULD_STOP.load(Ordering::Relaxed) {
-        let time_left = timeout.saturating_sub(start.elapsed());
-        let min_dur = Duration::from_millis(10);
-        if let Err(e) = pb.poll(std::cmp::min(time_left, min_dur)) {
-            // Error polling perf buffer: Interrupted system call (os error 4)
-            if e.kind() == libbpf_rs::ErrorKind::Interrupted {
-                continue;
+        // TODO split error
+        let open_obj = match ObjectBuilder::default().open_memory(program) {
+            Ok(o) => o,
+            Err(_) => {
+                let captured_log = VERIFIER_LOG.lock().unwrap().clone();
+                return Err(GuestMessage::LoadFail(captured_log));
             }
-            eprintln!("Error polling perf buffer: {}", e);
-            break;
+        };
+
+        let mut obj = match open_obj.load() {
+            Ok(o) => o,
+            Err(_) => {
+                let captured_log = VERIFIER_LOG.lock().unwrap().clone();
+                return Err(GuestMessage::VerifierFail(captured_log));
+            }
+        };
+
+        let mut links: Vec<_> = Vec::new();
+        let mut program_details: Vec<_> = Vec::new();
+
+        for p in obj.progs_mut() {
+            program_details.push(ProgramDetails {
+                name: p.name().to_string_lossy().to_string(),
+                section: p.section().to_string_lossy().to_string(),
+            });
+            let link = p.attach().unwrap(); // TODO
+            links.push(link);
+        }
+        if links.len() == 0 {
+            return Err(GuestMessage::NoProgramsFound);
+        }
+
+        let mut pb: Option<RingBuffer> = None;
+
+        for m in obj.maps_mut() {
+            if m.map_type() == MapType::RingBuf && m.name().to_string_lossy() == "_ep_debug_events"
+            {
+                let mut rpb = RingBufferBuilder::new();
+                rpb.add(&m, ev_closure).unwrap();
+                pb = Some(rpb.build().unwrap());
+                break;
+            }
+        }
+
+        if let Some(pb) = pb {
+            Ok(EbpfLoader {
+                pb,
+                _links: links,
+                program_details,
+            })
+        } else {
+            Err(GuestMessage::DebugMapNotFound)
         }
     }
-    tx.send(GuestMessage::Finished).unwrap();
+    pub fn run(&self, timeout: Duration) {
+        let start = Instant::now();
+        while start.elapsed() < timeout && !crate::SHOULD_STOP.load(Ordering::Relaxed) {
+            let time_left = timeout.saturating_sub(start.elapsed());
+            let min_dur = Duration::from_millis(10);
+            if let Err(e) = self.pb.poll(std::cmp::min(time_left, min_dur)) {
+                // Error polling perf buffer: Interrupted system call (os error 4)
+                if e.kind() == libbpf_rs::ErrorKind::Interrupted {
+                    continue;
+                }
+                eprintln!("Error polling perf buffer: {}", e);
+                break;
+            }
+        }
+    }
 }
--- userspace/src/main.rs
+++ userspace/src/main.rs
@@ -1,11 +1,12 @@
-use shared::GuestMessage;
+use shared::{GuestMessage, HostMessage};
 use std::io::Write;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::time::Duration;
+use std::sync::mpsc::Sender;
 use vsock::{VMADDR_CID_HOST, VsockStream};
 
+use crate::ebpf::EbpfLoader;
+
 pub static SHOULD_STOP: AtomicBool = AtomicBool::new(false);
-pub static EBPF_READY: AtomicBool = AtomicBool::new(false);
 
 mod ebpf;
 mod exercises;
@@ -39,8 +40,7 @@ fn real_main() {
 
     let jh = std::thread::spawn(move || {
         while let Ok(v) = rx.recv() {
-            let _ = bincode::encode_into_std_write(&v, &mut s_send, config)
-                .expect("can't send over vsock");
+            bincode::encode_into_std_write(&v, &mut s_send, config).expect("can't send over vsock");
 
             if v.is_terminal() {
                 SHOULD_STOP.store(true, Ordering::Relaxed);
@@ -49,38 +49,59 @@ fn real_main() {
     });
 
     match bincode::decode_from_std_read::<shared::HostMessage, _, _>(&mut s_rcv, config) {
-        Ok(shared::HostMessage::ExecuteProgram {
-            exercise_id,
-            timeout,
-            program,
-            user_key,
-        }) => {
-            let exercise = exercises::get_exercise(exercise_id);
-            let answer = shared::get_answer(exercise_id, user_key);
-
-            exercise.setup(&answer);
-
-            let panic_tx = tx.clone();
-            let jh2 = std::thread::spawn(move || {
-                ebpf::run_program(&program, timeout, tx);
-            });
-
-            while !EBPF_READY.load(Ordering::Relaxed) {
-                std::thread::sleep(Duration::from_millis(1));
-            }
-
-            let panics = std::panic::catch_unwind(|| {
-                exercise.run(&answer);
-            });
-            if let Err(_) = panics {
-                let _ = panic_tx.send(GuestMessage::Crashed);
-            }
-            SHOULD_STOP.store(true, Ordering::Relaxed);
-            jh2.join().unwrap();
-        }
+        Ok(hm) => run_exercise(hm, tx),
         Err(e) => {
             eprintln!("Failed to deserialize HostMessage: {}", e);
         }
     }
     jh.join().unwrap();
+    println!("done with sending");
+}
+
+fn run_exercise(hm: HostMessage, tx: Sender<GuestMessage>) {
+    let shared::HostMessage::ExecuteProgram {
+        exercise_id,
+        timeout,
+        program,
+        user_key,
+    } = hm;
+    let exercise = exercises::get_exercise(exercise_id);
+    let answer = shared::get_answer(exercise_id, user_key);
+
+    let txer = tx.clone();
+    let r_closure = move |data: &[u8]| {
+        txer.send(GuestMessage::Event(data.into())).unwrap();
+        0
+    };
+
+    let e = match EbpfLoader::load_program(&program, r_closure) {
+        Ok(p) => p,
+        Err(e) => {
+            let _ = tx.send(e).unwrap();
+            return;
+        }
+    };
+
+    exercise.setup(&answer);
+    for pd in &e.program_details {
+        let _ = tx.send(GuestMessage::FoundProgram {
+            name: pd.name.clone(),
+            section: pd.section.clone(),
+        });
+    }
+
+    let panic_tx = tx.clone();
+    let jh2 = std::thread::spawn(move || {
+        e.run(timeout);
+        let _ = tx.send(GuestMessage::Finished);
+    });
+
+    let panics = std::panic::catch_unwind(|| {
+        exercise.run(&answer);
+    });
+    if let Err(_) = panics {
+        let _ = panic_tx.send(GuestMessage::Crashed);
+    }
+    SHOULD_STOP.store(true, Ordering::Relaxed);
+    jh2.join().unwrap();
 }
