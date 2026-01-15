930120feb5a26da134461d0b6b0584d3cc7ef0b8
David Ventura <david@davidv.dev>
2026-01-15 10:36:19 +0100

    impl noop themes

--- website/src/styles/theme.ts
+++ website/src/styles/theme.ts
@@ -1,4 +1,4 @@
-export const theme = {
+export const darkTheme = {
   bgPrimary: "#1e1f24",
   bgSecondary: "#1e1f24",
   codeBg: "#2a2e38",
@@ -7,4 +7,19 @@ export const theme = {
   accent: "#61afef",
   alertBg: "#2e1e1e",
   border: "#2a2e38",
+  navbarBg: "hsl(224, 14%, 16%)",
+  navbarBorder: "hsl(224, 10%, 10%)",
+} as const;
+
+export const lightTheme = {
+  bgPrimary: "#1e1f24",
+  bgSecondary: "#1e1f24",
+  codeBg: "#2a2e38",
+  textPrimary: "#d4d4d4",
+  textSecondary: "#888888",
+  accent: "#61afef",
+  alertBg: "#2e1e1e",
+  border: "#2a2e38",
+  navbarBg: "hsl(224, 14%, 16%)",
+  navbarBorder: "hsl(224, 10%, 10%)",
 } as const;
